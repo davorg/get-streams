76b6772949e986aae639bc10384792283dca9140
Dave Cross <dave@dave.org.uk>
2008-01-26 18:49:29 +0000

    Changed version number to deal with svn revisions

--- BBCRobot.pm
+++ BBCRobot.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use LWP::RobotUA;
 
-our $VERSION = sprintf "%d.%02d", '$Revision$ ' =~ /(\d+)\.(\d+)/;
+our $VERSION = sprintf "%d.%02d", '$Revision$ ' =~ /(\d+)/;
 our @ISA = qw(LWP::RobotUA);
 
 my %defaults = (
